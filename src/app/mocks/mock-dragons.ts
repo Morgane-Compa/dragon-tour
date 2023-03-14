e879355b37adb5e138c7e46de3657a5b7c723848
Morgane-Compa <morgane.compafloury@gmail.com>
2023-03-14 16:03:17 +0100

    liste généré

---
+++ src/app/mocks/mock-dragons.ts
@@ -0,0 +1,40 @@
+import { dragon } from "./dragons";
+
+export const DRAGONS: dragon[] = [
+    {
+        id: 12,
+        name: 'Gogmazios'
+    },
+    {
+        id: 13,
+        name: 'Gore Magala'
+    },
+    {
+        id: 14,
+        name: 'Shagaru Magala'
+    },
+    {
+        id: 15,
+        name: 'Nergigante'
+    },
+    {
+        id: 16,
+        name: 'Kulve Taroth'
+    },
+    {
+        id: 17,
+        name: 'Xeno Jiva'
+    },
+    {
+        id: 18,
+        name: 'Safi Jiva'
+    },
+    {
+        id: 19,
+        name: 'Velkana'
+    },
+    {
+        id: 20,
+        name: 'Kushala Daora'
+    },
+]
